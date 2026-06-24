aabb1ae8398bea46f3e20d05dcb4099e933a9efe
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2026-06-24 08:39:06 +0530

    feat: connect mongodb database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,17 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js"
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json())
+
+await connectDB();
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
