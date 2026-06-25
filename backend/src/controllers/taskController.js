cfdd14d53876e69150569fe7a259e197099a1e14
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2026-06-25 10:57:25 +0530

    feat: implement task update endpoint

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -18,3 +18,49 @@ export const createTask = async (req, res) => {
         res.status(500).json({ message: 'Error creating task', error: error.message})
     }
 }
+
+export const getTasks = async (req, res) => {
+    try {
+        let tasks;
+
+        if (req.user.role === 'admin') {
+            tasks = await Task.find().populate('createdBy', 'name email').populate('assignedTo', 'name email').sort({ createdAt: -1})
+        } else {
+            tasks = await Task.find({$or: [{ createdBy: req.user._id }, { assignedTo: req.user._id}]}).populate('createdBy', 'name email').populate('assignedTo', 'name email').sort({ createdAt: -1})
+        }
+
+        res.status(200).json(tasks)
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching tasks', error: error.message})
+    }
+}
+
+export const updateTask = async (req, res) => {
+    try {
+        const { title, description, priority, status, dueDate, assignegTo } = req.body
+
+        const task = await Task.findById(req.params.id)
+
+        if (!task) {
+            return res.status(404).json({ message: 'Task not found'})
+        }
+
+        if (task.createdBy.toString() !== req.user._id.toString() && req.user.role !== 'admin') {
+            return res.status(403).json({ message: 'Access denied'})
+        }
+
+        task.title = title ?? task.title;
+        task.description =  description ?? task.description;
+        task.priority = priority ?? task.priority;
+        task.status = status ?? task.status;
+        task.dueDate = dueDate ?? task.dueDate;
+
+        await task.save()
+
+        const updatedTask = await Task.findById(req.params.id).populate('createdBy', 'name email').populate('assignedTo', 'name email')
+
+        res.status(200).json(updatedTask)
+    } catch (error) {
+        res.status(500).json({ message: 'Error updating task', error: error.message})
+    }
+}
--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createTask } from '../controllers/taskController.js'
+import { createTask, getTasks, updateTask } from '../controllers/taskController.js'
 import protect from '../middleware/auth.js'
 
 const router = express.Router()
 
 router.post('/', protect, createTask)
+router.get('/', protect, getTasks)
+router.put('/:id', protect, updateTask)
 
 export default router;
